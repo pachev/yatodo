c1ec36b6bfc409a7d904485013c532c5e1c2de27
Pachev Joseph <pachevjoseph@gmail.com>
2017-04-13 14:18:21 -0400

    API: adds jwt authentication and retrieval

--- api/src/main/java/yatodo/Config/WebSecurityConfig.java
+++ api/src/main/java/yatodo/Config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package yatodo.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by pachevjoseph on 4/12/17.
@@ -15,10 +18,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
-                .anyRequest()
-                .fullyAuthenticated().and().
-                httpBasic().and().
-                csrf().disable();
+                .antMatchers("/api").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/login","api/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
+            }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account for debug purpose
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
+
     }
 }
---
+++ api/src/main/java/yatodo/Model/AccountCredentials.java
@@ -0,0 +1,25 @@
+package yatodo.Model;
+
+/**
+ * Created by pachev on 4/13/17.
+ */
+public class AccountCredentials {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
