586ee13940051bc556a745de2cefa30cda9cb45d
Pachev Joseph <pachevjoseph@gmail.com>
2017-04-12 22:11:45 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Yatodo
+
+### Yet Another Todo app
+
+This is another Todo web app which uses Spring for the API and AngularJS 2 for the 
+frontend. 
+
+Click [here][1] for a running demo.
+
+
+## Running as a whole
+
+Each part can be ran individually. Refer to the READMEs inside the api project and the 
+frontend project for further documentation.
+
+
+## Running With Docker
+
+
+## 
+
---
+++ api/src/main/java/yatodo/YatodoApplication.java
@@ -0,0 +1,17 @@
+package yatodo;
+
+/**
+ * Created by pachevjoseph on 4/13/17.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatodoApplication {
+
+    //Simple default configurations to statrt this web application
+    public static void main(String[] args) {
+        SpringApplication.run(YatodoApplication.class, args);
+    }
+}
