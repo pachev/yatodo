3e24b8df2125a748e5bce5db2ed2da707b6da0bd
Pachev Joseph <pachevjoseph@gmail.com>
2017-04-15 13:39:56 -0400

    Frontend: adds initial styling to login and register

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from '../models/todo';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+    todos: Todo[];
 
   constructor() { }
 
